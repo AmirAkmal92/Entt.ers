46f61087d1a78659dfc8582b9589a871048a2829
azrolmn <azrolmn@gmail.com>
2018-02-24 17:06:22 +0800

    hip vs hop report

--- web/Controllers/VersusController.HipVsHop.cs
+++ web/Controllers/VersusController.HipVsHop.cs
@@ -66,6 +66,31 @@ namespace Entt.Ers.Controllers
                     new ReportParameter("day", day),
                     new ReportParameter("branchCode", branchCode)
                 };
+            reportViewer.LocalReport.EnableHyperlinks = true;
+            reportViewer.LocalReport.SetParameters(parameters);
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            ViewBag.TotalRows = dataset.Tables[0].Rows.Count;
+            ViewBag.ReportViewer = reportViewer;
+            var model = new PrefixReportViewModel { ReportDate = reportDate, ReportDay = day, SelectedBranch = branchCode };
+            return View(model);
+        }
+
+        public ActionResult HipVsNoHopDetails(string branchCode, double date, string day = "7")
+        {
+            var reportDate = DateTime.FromOADate(date);
+            ViewBag.Branches = m_enttContext.GetBranchInfo(branchCode).Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            ViewBag.ReportDays = ApplicationHelper.GetReportDays().Select(w => new SelectListItem { Text = w.Value, Value = w.Key.ToString() });
+            var reportViewer = ReportEngine.Create();
+
+            var dataset = m_enttContext.GetHipVsNoHopDetailsReportDataSet(reportDate.Date, int.Parse(day), branchCode);
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\HipVsNoHopDetails.rdlc";
+            var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", reportDate.ToShortDateString()),
+                    new ReportParameter("day", day),
+                    new ReportParameter("branchCode", branchCode)
+                };
+            reportViewer.LocalReport.EnableHyperlinks = true;
             reportViewer.LocalReport.SetParameters(parameters);
             reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
             ViewBag.TotalRows = dataset.Tables[0].Rows.Count;
