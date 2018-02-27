1545f5a34bf8f1f5fa5817a69e21c7e29fd3dd3a
azrolmn <azrolmn@gmail.com>
2018-02-27 14:49:37 +0800

    report snb

---
+++ web/Controllers/SalesBillingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Entt.Ers.Controllers
+{
+    [Authorize(Roles = "SNB")]
+    public class SalesBillingController : ApplicationBaseController
+    {
+        public ActionResult Financial()
+        {
+            return View();
+        }
+
+        public ActionResult Management()
+        {
+            return View();
+        }
+    }
+}
