980323990490a9a6babfc68d449fedd1013dd49b
azrolmn <azrolmn@gmail.com>
2018-05-17 14:27:37 +0800

    item over declared ipc report

---
+++ web/Controllers/VersusController.Ipc.cs
@@ -0,0 +1,220 @@
+﻿using Entt.Ers.Models;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Entt.Ers.Controllers
+{
+    public partial class VersusController
+    {
+        public ActionResult ItemOverDeclared()
+        {
+            var reportViewer = ReportEngine.Create();
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.ReportDays = ApplicationHelper.GetReportDays().Select(w => new SelectListItem { Text = w.Value, Value = w.Key.ToString() });
+            var model = new StandardReportViewModel { ReportDate = DateTime.Today };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ItemOverDeclared(StandardReportViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                KeepSessionAlive = false,
+                ProcessingMode = ProcessingMode.Local,
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100)
+            };
+
+            if (ModelState.IsValid)
+            {
+                DataSet dataset;
+                dataset = model.SelectedBranch == "All" ?
+                        m_enttContext.GetIpcItemOverDeclaredDataSet(model.ReportDate) :
+                        m_enttContext.GetIpcItemOverDeclaredBranchDataSet(model.ReportDate, model.SelectedBranch);
+                reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcItemOverDeclared.rdlc";
+
+                var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", model.ReportDate.ToShortDateString())
+                };
+
+                reportViewer.LocalReport.EnableHyperlinks = true;
+                reportViewer.LocalReport.SetParameters(parameters);
+                reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            }
+
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            return View(model);
+        }
+
+        public ActionResult ItemOverDeclaredDetails(string branchCode, double date)
+        {
+            var reportDate = DateTime.FromOADate(date);
+            ViewBag.Branches = m_enttContext.GetBranchInfo(branchCode).Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            var reportViewer = ReportEngine.Create();
+
+            var dataset = m_enttContext.GetIpcItemOverDeclaredBranchDetailsDataSet(reportDate.Date, branchCode);
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcItemOverDeclaredDetails.rdlc";
+            var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", reportDate.ToShortDateString()),
+                    new ReportParameter("branchCode", branchCode)
+                };
+            reportViewer.LocalReport.EnableHyperlinks = true;
+            reportViewer.LocalReport.SetParameters(parameters);
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            ViewBag.TotalRows = dataset.Tables[0].Rows.Count;
+            ViewBag.ReportViewer = reportViewer;
+            var model = new StandardReportViewModel { ReportDate = reportDate, SelectedBranch = branchCode };
+            return View(model);
+        }
+
+        public ActionResult ItemUnderDeclared()
+        {
+            var reportViewer = ReportEngine.Create();
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.ReportDays = ApplicationHelper.GetReportDays().Select(w => new SelectListItem { Text = w.Value, Value = w.Key.ToString() });
+            var model = new StandardReportViewModel { ReportDate = DateTime.Today };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ItemUnderDeclared(StandardReportViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                KeepSessionAlive = false,
+                ProcessingMode = ProcessingMode.Local,
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100)
+            };
+
+            if (ModelState.IsValid)
+            {
+                DataSet dataset;
+                dataset = model.SelectedBranch == "All" ?
+                        m_enttContext.GetIpcItemUnderDeclaredDataSet(model.ReportDate) :
+                        m_enttContext.GetIpcItemUnderDeclaredBranchDataSet(model.ReportDate, model.SelectedBranch);
+                reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcItemUnderDeclared.rdlc";
+
+                var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", model.ReportDate.ToShortDateString())
+                };
+
+                reportViewer.LocalReport.EnableHyperlinks = true;
+                reportViewer.LocalReport.SetParameters(parameters);
+                reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            }
+
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            return View(model);
+        }
+
+
+        public ActionResult ItemUnderDeclaredDetails(string branchCode, double date)
+        {
+            var reportDate = DateTime.FromOADate(date);
+            ViewBag.Branches = m_enttContext.GetBranchInfo(branchCode).Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            var reportViewer = ReportEngine.Create();
+
+            var dataset = m_enttContext.GetIpcItemUnderDeclaredBranchDetailsDataSet(reportDate.Date, branchCode);
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcItemUnderDeclaredDetails.rdlc";
+            var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", reportDate.ToShortDateString()),
+                    new ReportParameter("branchCode", branchCode)
+                };
+            reportViewer.LocalReport.EnableHyperlinks = true;
+            reportViewer.LocalReport.SetParameters(parameters);
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            ViewBag.TotalRows = dataset.Tables[0].Rows.Count;
+            ViewBag.ReportViewer = reportViewer;
+            var model = new StandardReportViewModel { ReportDate = reportDate, SelectedBranch = branchCode };
+            return View(model);
+        }
+
+        public ActionResult IpcNoAcceptance()
+        {
+            var reportViewer = ReportEngine.Create();
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.ReportDays = ApplicationHelper.GetReportDays().Select(w => new SelectListItem { Text = w.Value, Value = w.Key.ToString() });
+            var model = new StandardReportViewModel { ReportDate = DateTime.Today };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult IpcNoAcceptance(StandardReportViewModel model)
+        {
+            var reportViewer = new ReportViewer()
+            {
+                KeepSessionAlive = false,
+                ProcessingMode = ProcessingMode.Local,
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100)
+            };
+
+            if (ModelState.IsValid)
+            {
+                DataSet dataset;
+                dataset = model.SelectedBranch == "All" ?
+                        m_enttContext.GetIpcNoAcceptanceDataSet(model.ReportDate) :
+                        m_enttContext.GetIpcNoAcceptanceBranchDataSet(model.ReportDate, model.SelectedBranch);
+                reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcNoAcceptance.rdlc";
+
+                var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", model.ReportDate.ToShortDateString())
+                };
+
+                reportViewer.LocalReport.EnableHyperlinks = true;
+                reportViewer.LocalReport.SetParameters(parameters);
+                reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            }
+
+            ViewBag.ReportViewer = reportViewer;
+            ViewBag.Branches = GetUserViewBranches().Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            return View(model);
+        }
+
+        public ActionResult IpcNoAcceptanceDetails(string branchCode, double date)
+        {
+            var reportDate = DateTime.FromOADate(date);
+            ViewBag.Branches = m_enttContext.GetBranchInfo(branchCode).Select(w => new SelectListItem { Text = w.Name, Value = w.Code });
+            var reportViewer = ReportEngine.Create();
+
+            var dataset = m_enttContext.GetIpcNoAcceptanceBranchDetailsDataSet(reportDate.Date, branchCode);
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Versus\IpcNoAcceptanceDetails.rdlc";
+            var parameters = new List<ReportParameter>
+                {
+                    new ReportParameter("reportDate", reportDate.ToShortDateString()),
+                    new ReportParameter("branchCode", branchCode)
+                };
+            reportViewer.LocalReport.EnableHyperlinks = true;
+            reportViewer.LocalReport.SetParameters(parameters);
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataset.Tables[0]));
+            ViewBag.TotalRows = dataset.Tables[0].Rows.Count;
+            ViewBag.ReportViewer = reportViewer;
+            var model = new StandardReportViewModel { ReportDate = reportDate, SelectedBranch = branchCode };
+            return View(model);
+        }
+
+    }
+}
---
+++ web/Models/EnttReportDataContext.Ipc.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Entt.Ers.Models
+{
+    public partial class EnttReportDataContext
+    {
+        public DataSet GetIpcItemOverDeclaredDataSet(DateTime reportDate)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_over_declared_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcItemOverDeclaredBranchDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_over_declared_branch_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 50).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcItemOverDeclaredBranchDetailsDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_over_declared_branch_details]", conn))
+            {
+                cmd.CommandTimeout = 80;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 10).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcItemUnderDeclaredDataSet(DateTime reportDate)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_under_declared_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcItemUnderDeclaredBranchDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_under_declared_branch_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 50).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcItemUnderDeclaredBranchDetailsDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_item_under_declared_branch_details]", conn))
+            {
+                cmd.CommandTimeout = 80;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 10).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcNoAcceptanceDataSet(DateTime reportDate)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_no_acceptance_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcNoAcceptanceBranchDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_no_acceptance_branch_summary]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 50).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+
+        public DataSet GetIpcNoAcceptanceBranchDetailsDataSet(DateTime reportDate, string branchCode)
+        {
+            var dataset = new DataSet();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["EnttConnectionString"].ConnectionString))
+            using (var cmd = new SqlCommand("[Entt].[ipc_no_acceptance_branch_details]", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@reportDate", SqlDbType.Date).Value = reportDate;
+                cmd.Parameters.Add("@branchCode", SqlDbType.NVarChar, 50).Value = branchCode;
+                var sqlDataAapter = new SqlDataAdapter(cmd);
+                sqlDataAapter.Fill(dataset);
+            }
+            return dataset;
+        }
+    }
+}
